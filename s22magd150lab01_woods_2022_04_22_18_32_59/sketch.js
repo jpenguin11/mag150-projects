557006cffbdb36f3de68f812d337c1f8475b92d3
jpenguin11 <104222662+jpenguin11@users.noreply.github.com>
2022-04-22 13:33:27 -0500

    Add files via upload

---
+++ s22magd150lab01_woods_2022_04_22_18_32_59/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(200, 200);
+  background (125);
+  
+}
+
+function draw() {
+  
+  rect( 40, 55, 120,120);
+  fill(255)
+  rect(50, 115, 25, 23)
+  strokeCap(24);
+  ellipse(100, 75, 35, 35);
+  ellipse(100, 135, 12,12);
+  fill (255)
+  rect(125, 115, 25, 23);
+  fill(65);
+  line(29, 61, 96, 19);
+  line(171, 61, 95, 19);
+  line ( 89, 175, 89, 120);
+  stroke (0);
+  line (113, 175, 112, 120);
+  line (112, 119, 89, 119);
+  strokeWeight(3);
+  point( 110, 149);
+  point (120, 139);
+  
+   }
+  
+  
+
+                 
+
